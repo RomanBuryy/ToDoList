8bc6165420a59163dae07b5defbe00a14d1d8c58
RomanBuryy <roman.buriy@gmail.com>
2018-08-29 12:44:08 +0300

    It's working))

--- ToDoList/todo_app/forms.py
+++ ToDoList/todo_app/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 
 class TodoForm(forms.Form):
-    text = forms.CharField(max_lenght = 50,
+    text = forms.CharField(max_length = 50,
     widget = forms.TextInput(
     attrs = {
-
+        'class': 'form-control',
+        'placeholder': 'A Django TODO'
     }
     ))
